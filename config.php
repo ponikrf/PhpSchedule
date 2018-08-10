c490e6349dc993ddad22b613abd1dd2a5746aa03
Борис Бобылев <ponik_rf@mail.ru>
2018-08-10 15:54:46 +0300

    Git WTF?

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
 return [
     'database' => [
-        'username' => 'user',
-        'password' => 'password',
+        'username' => 'root',
+        'password' => 'lehfktq',
         'hostname' => '127.0.0.1',   // IP address if localhost!
-        'database' => 'scheduler',
+        'database' => 'testSch',
     ],
 ];
